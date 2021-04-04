00d42d4b3c65c919be1586ed2ea1345447e1e2e1
Henry <henrycaba471@gamil.com>
2021-04-04 14:22:30 -0500

    he agregado un campo para clear y sus funciones

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const contarCaracteres = ()=>{
     let descripcion = document.getElementById('resultado');
     (caracteresTexto.length == '')
     ? descripcion.innerHTML=('No has ingresado ningun texto :(')
-    : descripcion.innerHTML=(`En el texto "${caracteresTexto}" hay ${caracteresTexto.length} caracteres`);
+    : descripcion.innerHTML=(`En el texto "${caracteresTexto}" hay ${caracteresTexto.length} caracteres :)`);
 }
 
+let caracteresTexto = document.getElementById('entrada');
+let borrar = document.getElementById('clear');
+let descripcion = document.getElementById('resultado');
+borrar.addEventListener('click',()=>{
+    caracteresTexto.value = '';
+    descripcion.innerHTML=(':)')
+});
