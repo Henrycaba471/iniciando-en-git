e2022306e3479b37d3ce8167ee1e8f768eb4b834
Henry Cañizares <henrycaba471@gamil.con>
2021-04-04 11:52:40 -0500

    actualizando la parte verdadera

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const contarCaracteres = ()=>{
     let caracteresTexto = document.getElementById('entrada').value;
     let descripcion = document.getElementById('resultado');
     (caracteresTexto.length == '')
-    ? descripcion.innerHTML=('No has ingresado ningun texto')
+    ? descripcion.innerHTML=('No has ingresado ningun texto :(')
     : descripcion.innerHTML=(`En el texto "${caracteresTexto}" hay ${caracteresTexto.length} caracteres`);
 }
 
