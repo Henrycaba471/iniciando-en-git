0ea712483494a7d03e8c2645571ad2e885f88ae6
Henry <henrycaba471@gamil.com>
2021-04-04 18:37:38 -0500

    Nuevo ejercicio incluido

--- app.js
+++ app.js
@@ -15,3 +15,6 @@ borrar.addEventListener('click',()=>{
     caracteresTexto.value = '';
     descripcion.innerHTML=(':)')
 });
+
+//2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+
