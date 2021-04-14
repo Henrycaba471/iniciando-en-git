51bc9a12055954689ec7d62bde348ce4ef83e2b7
henrycaba471 <henrycaba471@gmail.com>
2021-04-13 23:03:07 -0500

    Ejercicio 15

--- app.js
+++ app.js
@@ -389,3 +389,5 @@ borrar14.addEventListener('click', () => {
     unidad.value = '';
     resultado14.innerText = (':)');
 });
+
+//15) Programa una función para convertir números de base binaria a decimal y viceversa, pe. miFuncion(100,2) devolverá 4 base 10.
